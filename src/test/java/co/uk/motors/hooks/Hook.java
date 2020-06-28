cf6a071fa21edc10f0baa370724ba4dd016480e8
DONKRISS <osubor101@gmail.com>
2020-06-28 02:11:44 +0100

    MOT-08 - Created browser manager and its set ups

--- src/test/java/co/uk/motors/hooks/Hook.java
+++ src/test/java/co/uk/motors/hooks/Hook.java
@@ -1,3 +1,20 @@
 package co.uk.motors.hooks;
 
-//public class Hook extends Browserm  {}
+import co.uk.motors.commons.BrowserManager;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Hook extends BrowserManager
+{
+    @Before
+    public void setup()
+    {
+        launchBrowser("Chrome");
+    }
+
+    @After
+    public void  tearDown()
+    {
+        closeBrowser();
+    }
+}
